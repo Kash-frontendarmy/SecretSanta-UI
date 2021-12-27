381e49547174d6da9182ce42bce068a64d0f5904
hardik-1902 <94845670+Kash-frontendarmy@users.noreply.github.com>
2021-12-27 12:10:52 +0530

    base url change

--- src/rest/index.js
+++ src/rest/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:9092";
+const BASE_URL = "https://secretsanta-api.herokuapp.com";
 
 
 export const addParticipant = async (data) => {
